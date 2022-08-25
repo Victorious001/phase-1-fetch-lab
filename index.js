ea3c61b89fbf2fbf510267d4e612112bbbf8385c
Victorious <new@mail.com>
2022-08-25 16:35:45 +0300

    commit tree 5c5d716fbccafc4cd330893efdbcc8a66cc6c29c

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+    // To pass the tests, don't forget to return your fetch!
+    fetch('https://anapioficeandfire.com/api/books')
+        .then(function(response) {
+            console.log(response)
+            return response.json()
+        })
+        .then(function renderBooks(response) {
+            console.log(response)
+            console.log(renderBooks)
+        })
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const main = document.querySelector('main');
+    books.forEach(book => {
+        const h2 = document.createElement('h2');
+        h2.innerHTML = book.name;
+        main.appendChild(h2);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+    fetchBooks();
+})
